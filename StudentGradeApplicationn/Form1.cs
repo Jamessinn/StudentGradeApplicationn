1f1f5b1235bf9d2ac473f7010f0ad646b6021561
carrascal.296800 <carrascal.296800@LAB-A-PC21>
2024-02-23 09:54:57 +0800

    Add project files.

---
+++ StudentGradeApplicationn/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace StudentGradeApplicationn
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double ScienceSubject = Convert.ToDouble(textBox2.Text);
+            double MathSubject = Convert.ToDouble(textBox3.Text);
+            double PESubject = Convert.ToDouble(textBox4.Text);
+            double ValuesSubject = Convert.ToDouble(textBox5.Text);
+            double HistorySubject = Convert.ToDouble(textBox6.Text);
+
+            double totalGrade = ScienceSubject  + MathSubject + PESubject + ValuesSubject + HistorySubject;
+            double gradeAverage = totalGrade / 5;
+
+            label7.Text = gradeAverage >= 75.00 ? "            The Student passed\n        " + " The general average of " + textBox1.Text + " is " + gradeAverage : "               The Student Failed\n                " + "The general average of " + textBox1.Text + " is " + gradeAverage;
+
+        }
+    }
+}
